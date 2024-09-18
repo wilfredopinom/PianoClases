a2fa0ef96f23d3a4744c15fdbfd10de2bc59833b
wilfredopinom <wilfredopinom@gmail.com>
2024-09-18 13:20:33 +0200

    App Final

--- main.js
+++ main.js
@@ -2,8 +2,24 @@ import getNoteFromkey from './noteUtils';
 import './style.css'
 import * as Tone from "tone";
 
-const synth = new Tone.Synth().toDestination();
-
+//const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Sampler({
+    urls: {
+        C4: "C4.mp3",
+        "C#4": "Cs4.mp3",
+        D4: "D4.mp3",
+        "D#4": "Ds4.mp3",
+        E4: "E4.mp3",
+        F4: "F4.mp3",
+        "F#4": "Fs4.mp3",
+        G4: "G4.mp3",
+        "G#4": "Gs4.mp3",
+        A4: "A4.mp3", "A#4": "As4.mp3",
+        C5: "C5.mp3"  
+    },
+    release: 1,
+   //  baseUrl: "https://tonejs.github.io/audio/salamander/",
+}).toDestination();
 
 
 let keys = document.querySelectorAll('.key')
